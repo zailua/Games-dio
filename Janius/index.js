7053c2bbaaa7ddac09b5d021baa16c91cdd34393
zailua <84216764+zailua@users.noreply.github.com>
2022-05-05 05:26:09 -0300

    update the bugs

--- Janius/index.js
+++ Janius/index.js
@@ -9,9 +9,9 @@ const yellow = document.querySelector('.yellow')
 
 let shuffleOrder = () => {
 	let colorOrder = Math.floor(Math.random() * 4)
-
 	order[order.length] = colorOrder
 	clickedOrder = []
+
 	for (let i in order) {
 		let elementColor = createColorElement(order[i])
 		lightColor(elementColor, Number(i) + 1)
@@ -31,23 +31,22 @@ let lightColor = (element, number) => {
 let checkOrder = () => {
 	for (let i in clickedOrder) {
 		if (clickedOrder[i] != order[i]) {
-			lose()
+			gameOver()
 			break
 		}
 	}
 	if (clickedOrder.length == order.length) {
-		alert(`Pontuação: ${score}\nYou Won! Begin the hard level!`)
+		alert(`Pontuação: ${score}\nYou Won! Here we go Again!`)
 		nextLevel()
 	}
 }
 
 let click = color => {
 	clickedOrder[clickedOrder.length] = color
-	elementColor(color).classList.add('selected')
+	createColorElement(color).classList.add('selected')
 
 	setTimeout(() => {
-		elementColor(color).classList.remove('selected')
-
+		createColorElement(color).classList.remove('selected')
 		checkOrder()
 	}, 250)
 }
@@ -69,8 +68,9 @@ let nextLevel = () => {
 	shuffleOrder()
 }
 
+//funcao para game over
 let gameOver = () => {
-	alert(`Pontuação: ${score}!\ Já era!\n Aw Shit Let's go Again!`)
+	alert(`Pontuação: ${score}!\nYou Lose!\n Aw sh*t here we go again`)
 	order = []
 	clickedOrder = []
 
@@ -78,7 +78,7 @@ let gameOver = () => {
 }
 
 let playGame = () => {
-	alert('Bem Vindo ao Janius! Aw Shit Here we go again...')
+	alert('Bem vindo ao Gênesis! Iniciando novo jogo!')
 	score = 0
 
 	nextLevel()
